4687c4ec14402de8921ca9cb66a8301fc4b69210
Drago <pedrodrago.profissional@gmail.com>
2022-06-20 17:00:38 -0300

    Add the draw funtionality by hovering the divs

--- script.js
+++ script.js
@@ -1,6 +1,24 @@
-let master=document.querySelector('#master');
+// let master=document.querySelector('#master');
 
 
-for(let i=0; i<250;i++){
-    master.appendChild(document.createElement('div'));
+// for(let i=0; i<256;i++){
+//     master.appendChild(document.createElement('div'));
+// }
+
+
+let master=document.querySelector("#master");
+
+for(let i=0; i<256; i++){
+    let divs = document.createElement('div');
+    divs.classList.add('pixel');
+    master.appendChild(divs)
 }
+
+let divs = document.querySelectorAll(".pixel")
+
+divs.forEach(div => {
+    div.addEventListener('mouseover', function hover(){
+        div.classList.add('hover')
+    })
+}
+)
