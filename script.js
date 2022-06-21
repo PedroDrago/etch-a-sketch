c871e304084c39dc2c0c8a6572cf144dbeb73cc9
Drago <pedrodrago.profissional@gmail.com>
2022-06-21 06:13:02 -0300

    Put grid generation inside a function

--- script.js
+++ script.js
@@ -1,18 +1,22 @@
 let grid=document.querySelector("#grid");
 
 
-size = 16*16
 
-for(let i=0; i<size; i++){
-    let divs = document.createElement('div');
-    divs.classList.add('square');
-    grid.appendChild(divs)
+function generateGrid(size){
+
+    for(let i=0; i<size; i++){
+        let divs = document.createElement('div');
+        divs.classList.add('square');
+        grid.appendChild(divs)
+    }
+
+    
+    let divs = document.querySelectorAll(".square");
+    divs.forEach(div => {
+        div.addEventListener('mouseover', function hover(){
+            div.classList.add('hover')
+        })
+    })
 }
 
-let divs = document.querySelectorAll(".square")
-
-divs.forEach(div => {
-    div.addEventListener('mouseover', function hover(){
-        div.classList.add('hover')
-    })
-})
+generateGrid(16*16)
